ad49b5698ff2b2ba8ed19459084cec17bd9485f2
Pete Hanner <petehanner@gmail.com>
2019-08-06 16:32:09 -0400

    Complete(ish)

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,8 +3,9 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character_name)
-  #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  # make the web request
+  url = "https://www.swapi.co/api/people/?search=#{character_name}"
+  response_string = RestClient.get(url)
   response_hash = JSON.parse(response_string)
 
   # iterate over the response hash to find the collection of `films` for the given character`
