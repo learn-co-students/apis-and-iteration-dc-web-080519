f95e376f5c2391b83256d58bc95b0e9c671d3e77
Pete Hanner <petehanner@gmail.com>
2019-08-06 15:39:17 -0400

    API communicator working, not the CLI

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -7,4 +7,5 @@ def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
   user_character = gets.chomp.downcase
+  print "\n"
 end
